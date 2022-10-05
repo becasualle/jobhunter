6cdbac7371c8533421fc45bdb63b11de52de7a59
Mark <mark.nelubin@gmail.com>
2022-10-05 20:58:35 +0300

    12-register-page-setup

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,68 +1,78 @@
-import main from '../assets/images/main.svg';
+import main from "../assets/images/main.svg";
 import styled from "styled-components";
-import { Logo } from '../components';
-import { Link } from 'react-router-dom';
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
 
-type Props = {}
+type Props = {};
 
 const Landing = (props: Props) => {
-    return (
-        <Wrapper>
-            <nav>
-                <Logo />
-            </nav>
-            <div className="container page">
-                <div className="info">
-                    <h1>job <span>tracking</span> app</h1>
-                    <p>Messenger bag air plant shoreditch tumblr lo-fi keytar lyft, pitchfork activated charcoal gluten-free street art twee tbh normcore. Disrupt hell of thundercats quinoa, activated charcoal salvia photo booth master cleanse tattooed put a bird on it pitchfork kitsch vice. </p>
-                    <Link to='/register' className="btn btn-hero">Login/Register</Link>
-                </div>
-                <img src={main} alt="job hunt" className='img main-img' />
-            </div>
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Messenger bag air plant shoreditch tumblr lo-fi keytar lyft,
+            pitchfork activated charcoal gluten-free street art twee tbh
+            normcore. Disrupt hell of thundercats quinoa, activated charcoal
+            salvia photo booth master cleanse tattooed put a bird on it
+            pitchfork kitsch vice.{" "}
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.main`
-    nav {
-        width: var(--fluid-width);
-        max-width: var(--max-width);
-        margin: 0 auto;
-        height: var(--nav-height);
-        display: flex;
-        align-items: center;
-    }
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
 
-    .page {
-        min-height: calc(100vh - var(--nav-height));
-        display: grid;
-        align-items: center;
-        margin-top:-3rem;
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
 
-        @media (min-width: 992px){
-            grid-template-columns: 1fr 1fr;
-            column-gap: 3rem;
-        }
+    @media (min-width: 992px) {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
     }
-    
-    h1 {
-        font-weight: 700;
-        
-        span {
-            color: var(--primary-500);
-        }
-    }
+  }
 
-    p {
-        color: var(--grey-600);
-    }
+  h1 {
+    font-weight: 700;
 
-    .main-img {
-        display: none;
-        @media (min-width: 992px){
-            display: block;
-        }
+    span {
+      color: var(--primary-500);
     }
-`
+  }
 
-export default Landing
+  p {
+    color: var(--grey-600);
+  }
+
+  .main-img {
+    display: none;
+    @media (min-width: 992px) {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,11 +1,87 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+import styled from "styled-components";
 
-type Props = {}
+type Props = {};
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = (props: Props) => {
-    return (
-        <div>Register</div>
-    )
-}
+  const [values, setValues] = useState(initialState);
 
-export default Register
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    console.log(e.target.value);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            id="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
+
+export default Register;
