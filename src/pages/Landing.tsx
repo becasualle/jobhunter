a41237a3c707664de76ea086c6e692edac1daac0
Mark <mark.nelubin@gmail.com>
2022-10-05 12:14:54 +0300

    5-Landing-basic-structure

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg';
-import main from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
 
 type Props = {}
 
 const Landing = (props: Props) => {
     return (
-        <h1>Landing page</h1>
+        <main>
+            <nav>
+                <img src={logo} alt="jobhunter logo" className='logo' />
+            </nav>
+            <div className="container page">
+                <div className="info">
+                    <h1>job <span>tracking</span> app</h1>
+                    <p>Messenger bag air plant shoreditch tumblr lo-fi keytar lyft, pitchfork activated charcoal gluten-free street art twee tbh normcore. Disrupt hell of thundercats quinoa, activated charcoal salvia photo booth master cleanse tattooed put a bird on it pitchfork kitsch vice. </p>
+                    <button className="btn btn-hero">Login/Register</button>
+                </div>
+                <img src={main} alt="job hunt" className='img main-img' />
+            </div>
+        </main>
     )
 }
 
