1fa797ce1e99d524ee6e3135f87e99abf070a625
Mark <mark.nelubin@gmail.com>
2022-10-05 12:51:18 +0300

    7-wrap-component-complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Landing from "./pages/Landing";
+import styled from 'styled-components';
+
 const App = () => {
   return (
-    <Landing />
+    <div>
+      <Landing />
+    </div>
   );
 }
 
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import styled from "styled-components";
 
 type Props = {}
 
 const Landing = (props: Props) => {
     return (
-        <main>
+        <Wrapper>
             <nav>
                 <img src={logo} alt="jobhunter logo" className='logo' />
             </nav>
@@ -18,8 +19,53 @@ const Landing = (props: Props) => {
                 </div>
                 <img src={main} alt="job hunt" className='img main-img' />
             </div>
-        </main>
+        </Wrapper>
     )
 }
+
+const Wrapper = styled.main`
+    nav {
+        width: var(--fluid-width);
+        max-width: var(--max-width);
+        margin: 0 auto;
+        height: var(--nav-height);
+        display: flex;
+        align-items: center;
+    }
+
+    .page {
+        min-height: calc(100vh - var(--nav-height));
+        display: grid;
+        align-items: center;
+        margin-top:-3rem;
+    }
+    
+    h1 {
+        font-weight: 700;
+        
+        span {
+            color: var(--primary-500);
+        }
+    }
+
+    p {
+        color: var(--grey-600);
+    }
+
+    .main-img {
+        display: none;
+    }
+
+    @media (min-width: 992px){
+        .page {
+            grid-template-columns: 1fr 1fr;
+            column-gap: 3rem;
+        }
+
+        .main-img {
+            display: block;
+        }
+    }
+`
 
 export default Landing
