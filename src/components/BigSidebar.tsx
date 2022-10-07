08e9c5dfaee5aba92557a775959128347397c767
Mark <mark.nelubin@gmail.com>
2022-10-07 11:03:03 +0300

    25-Complete SharedLayout

---
+++ src/components/BigSidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const BigSidebar = (props: Props) => {
+  return <div>BigSidebar</div>;
+};
+
+export default BigSidebar;
---
+++ src/components/SmallSidebar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SmallSidebar = (props: Props) => {
+  return <div>SmallSidebar</div>;
+};
+
+export default SmallSidebar;
