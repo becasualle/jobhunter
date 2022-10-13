2ad10bb7b09d22878aa048d63de1336db0a4b452
Mark <mark.nelubin@gmail.com>
2022-10-13 09:02:23 +0300

    41-job Slice

---
+++ src/features/job/jobSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import customFetch from "../../utils/axios";
+
+interface JobState {
+  isLoading: boolean;
+  isEditing: boolean;
+  position: string;
+  company: string;
+  jobLocation: string;
+  jobTypeOptions: ["full-time", "part-time", "remote", "internship"];
+  jobType: "full-time" | "part-time" | "remote" | "internship";
+  statusOptions: ["interview", "declined", "pending"];
+  status: "interview" | "declined" | "pending";
+  editJob: string;
+}
+
+const initialState: JobState = {
+  isLoading: false,
+  isEditing: false,
+  position: "",
+  company: "",
+  jobLocation: "",
+  jobTypeOptions: ["full-time", "part-time", "remote", "internship"],
+  jobType: "full-time",
+  statusOptions: ["interview", "declined", "pending"],
+  status: "pending",
+  editJob: "",
+};
+
+export const jobSlice = createSlice({
+  name: "job",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {},
+});
+
+export default jobSlice.reducer;
