49b4de7c335abd21651de6baa1ee98f462f10ed6
Mark <mark.nelubin@gmail.com>
2022-10-19 14:40:20 +0300

    60-ShowStats

--- src/features/allJobs/allJobsSlice.ts
+++ src/features/allJobs/allJobsSlice.ts
@@ -5,23 +5,6 @@ import { RootState } from "../../app/store";
 import { JobType, StatusType } from "../job/jobSlice";
 import customFetch from "../../utils/axios";
 
-export interface JobsFiltersState {
-  search: string;
-  searchStatus: string;
-  searchType: string;
-  sort: string;
-  sortOptions: string[];
-}
-export interface JobsInitialState extends JobsFiltersState {
-  isLoading: boolean;
-  jobs: APIJob[];
-  totalJobs: number;
-  numOfPages: number;
-  page: number;
-  stats: {};
-  monthlyApplications: [];
-}
-
 export interface APIJob {
   _id: string;
   company: string;
@@ -39,6 +22,40 @@ export interface ApiJobsData {
   totalJobs: number;
   numOfPages: number;
   jobs: APIJob[];
+}
+
+export type Stats = {
+  pending: number;
+  interview: number;
+  declined: number;
+};
+
+export type MonthlyApplication = {
+  date: string;
+  count: number;
+};
+
+export interface ApiStatsInfo {
+  defaultStats: Stats;
+  monthlyApplications: MonthlyApplication[] | [];
+}
+
+export interface JobsFiltersState {
+  search: string;
+  searchStatus: string;
+  searchType: string;
+  sort: string;
+  sortOptions: string[];
+}
+
+export interface JobsInitialState extends JobsFiltersState {
+  isLoading: boolean;
+  jobs: APIJob[];
+  totalJobs: number;
+  numOfPages: number;
+  page: number;
+  stats: Stats | {};
+  monthlyApplications: MonthlyApplication[] | [];
 }
 
 const initialFilterState: JobsFiltersState = {
@@ -74,6 +91,19 @@ export const getAllJobs = createAsyncThunk(
     }
   }
 );
+
+export const showStats = createAsyncThunk(
+  "allJobs/showStats",
+  async (_, thunkApi) => {
+    try {
+      const resp = await customFetch("/jobs/stats");
+      return resp.data as ApiStatsInfo;
+    } catch (error) {
+      const err = error as AxiosError<{ msg: string }>;
+      return thunkApi.rejectWithValue(err.response?.data.msg);
+    }
+  }
+);
 const allJobsSlice = createSlice({
   name: "allJobs",
   initialState,
@@ -95,6 +125,18 @@ const allJobsSlice = createSlice({
         state.jobs = payload.jobs;
       })
       .addCase(getAllJobs.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload as string);
+      })
+      .addCase(showStats.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(showStats.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.stats = payload.defaultStats;
+        state.monthlyApplications = payload.monthlyApplications;
+      })
+      .addCase(showStats.rejected, (state, { payload }) => {
         state.isLoading = false;
         toast.error(payload as string);
       });
--- src/pages/Dashboard/Stats.tsx
+++ src/pages/Dashboard/Stats.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch } from "../../app/hooks";
+import { showStats } from "../../features/allJobs/allJobsSlice";
 
 type Props = {};
 
 const Stats = (props: Props) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(showStats());
+  }, [dispatch]);
   return <div>Stats</div>;
 };
 
