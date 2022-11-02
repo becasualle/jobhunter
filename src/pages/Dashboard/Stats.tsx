bbeb255818226b7c69993d673598d9f405a3fec7
Mark <mark.nelubin@gmail.com>
2022-11-02 17:55:42 +0700

    test

--- src/pages/Dashboard/Stats.tsx
+++ src/pages/Dashboard/Stats.tsx
@@ -3,9 +3,7 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { showStats } from "../../features/allJobs/allJobsSlice";
 import { StatsContainer, ChartsContainer, Loading } from "../../components";
 
-type Props = {};
-
-const Stats = (props: Props) => {
+const Stats = () => {
   const dispatch = useAppDispatch();
   const { isLoading, monthlyApplications } = useAppSelector(
     (store) => store.allJobs
